04e25b1dc32d80317449f93e0fa44bfa6b2b5613
whitejh <jhk0806@gmail.com>
2026-03-17 16:53:13 +0900

    feat: category 도메인 엔티티 추가

---
+++ src/main/java/com/jjanpot/server/domain/category/entity/CategoryName.java
@@ -0,0 +1,11 @@
+package com.jjanpot.server.domain.category.entity;
+
+public enum CategoryName {
+	FOOD_DELIVERY,    // 외식/배달
+	CAFE_DESSERT,     // 카페/디저트
+	TRANSPORT,        // 교통
+	FASHION_BEAUTY,   // 패션/뷰티
+	HOBBY_CULTURE,    // 취미/문화생활
+	ALCOHOL,          // 술/유흥
+	OTHER             // 기타
+}
