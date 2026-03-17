05acfec1d100cf09a3b1ea480d98c2f8c46a5b4c
whitejh <jhk0806@gmail.com>
2026-03-17 16:54:58 +0900

    feat: item, terms 도메인 엔티티 추가

---
+++ src/main/java/com/jjanpot/server/domain/terms/entity/Terms.java
@@ -0,0 +1,45 @@
+package com.jjanpot.server.domain.terms.entity;
+
+import com.jjanpot.server.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "terms")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Terms extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "terms_id")
+	private Long termsId;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "type", nullable = false)
+	private TermsType type;
+
+	@Column(name = "version", nullable = false, length = 20)
+	private String version;
+
+	@Column(name = "title", nullable = false, length = 200)
+	private String title;
+
+	@Column(name = "content", nullable = false,
+		columnDefinition = "TEXT")
+	private String content;
+}
---
+++ src/main/java/com/jjanpot/server/domain/terms/entity/TermsType.java
@@ -0,0 +1,5 @@
+package com.jjanpot.server.domain.terms.entity;
+
+public enum TermsType {
+	SERVICE_TERMS  // 서비스 이용약관
+}
