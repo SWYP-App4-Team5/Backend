5602c8ef16bbf005a4edf6306a6e616452e17a1c
whitejh <jhk0806@gmail.com>
2026-03-22 14:45:34 +0900

    feat: ENUM 파일에 displayName 추가

--- src/main/java/com/jjanpot/server/domain/category/entity/CategoryName.java
+++ src/main/java/com/jjanpot/server/domain/category/entity/CategoryName.java
@@ -1,11 +1,18 @@
 package com.jjanpot.server.domain.category.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum CategoryName {
-	FOOD_DELIVERY,    // 외식/배달
-	CAFE_DESSERT,     // 카페/디저트
-	TRANSPORT,        // 교통
-	FASHION_BEAUTY,   // 패션/뷰티
-	HOBBY_CULTURE,    // 취미/문화생활
-	ALCOHOL,          // 술/유흥
-	OTHER             // 기타
+	FOOD_DELIVERY("배달/외식"),
+	CAFE_DESSERT("카페/디저트"),
+	TRANSPORT("교통/자동차"),
+	FASHION_BEAUTY("패션/뷰티"),
+	HOBBY_CULTURE("취미/여가"),
+	ALCOHOL("술/유흥"),
+	OTHER("기타");
+
+	private final String displayName;
 }
