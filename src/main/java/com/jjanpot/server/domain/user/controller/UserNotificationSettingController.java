3d61a62c55503a5d8fd1da3878735b1332eb2e15
chaechae10 <chayeon0303@naver.com>
2026-04-03 19:42:51 +0900

    [Fix] 알림 설정 컨트롤러 이름 수정 - 기존 : NotificationControlle - 수정 후 :UserNorificationSettingController

--- src/main/java/com/jjanpot/server/domain/user/controller/UserNotificationSettingController.java
+++ src/main/java/com/jjanpot/server/domain/user/controller/UserNotificationSettingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jjanpot.server.domain.user.controller.docs.NotificationControllerDocs;
+import com.jjanpot.server.domain.user.controller.docs.UserNotificationSettingControllerDocs;
 import com.jjanpot.server.domain.user.dto.request.NotificationUpdateRequest;
 import com.jjanpot.server.domain.user.dto.response.NotificationResponse;
 import com.jjanpot.server.domain.user.service.UserService;
@@ -18,7 +18,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users/v1/notifications")
+<<<<<<<< HEAD:src/main/java/com/jjanpot/server/domain/user/controller/NotificationSettingController.java
 public class NotificationSettingController implements NotificationControllerDocs {
+========
+public class UserNotificationSettingController implements UserNotificationSettingControllerDocs {
+>>>>>>>> 7181fc9 ([Fix] 알림 설정 컨트롤러 이름 수정):src/main/java/com/jjanpot/server/domain/user/controller/UserNotificationSettingController.java
 
 	private final UserService userService;
 
--- src/main/java/com/jjanpot/server/domain/user/controller/docs/UserNotificationSettingControllerDocs.java
+++ src/main/java/com/jjanpot/server/domain/user/controller/docs/UserNotificationSettingControllerDocs.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "User - Notification", description = "알림 설정 API")
 @SecurityRequirement(name = "JWT TOKEN")
-public interface NotificationControllerDocs {
+public interface UserNotificationSettingControllerDocs {
 
 	@Operation(summary = "알림 설정 조회", description = "1일 1회 미인증 알림, 주간 미인증 알림, 마케팅 수신 동의 여부를 조회합니다.")
 	@ApiResponse(responseCode = "200", description = "알림 설정 조회 성공")
