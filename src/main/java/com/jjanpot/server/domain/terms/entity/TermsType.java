5d670eaa4edde7e532fae09fc9847e090a91719c
chaechae10 <chayeon0303@naver.com>
2026-04-05 01:02:59 +0900

    [Fix] 약관목록 Enum 타입 추가 - 운영 정책 삭제 및 이용가이드 추가

--- src/main/java/com/jjanpot/server/domain/terms/entity/TermsType.java
+++ src/main/java/com/jjanpot/server/domain/terms/entity/TermsType.java
@@ -3,6 +3,6 @@ package com.jjanpot.server.domain.terms.entity;
 public enum TermsType {
 	SERVICE_TERMS,      // 서비스 이용약관
 	PRIVACY_POLICY,     // 개인정보처리방침
-	OPERATION_POLICY,   // 운영 정책
-	MARKETING_CONSENT   // 마케팅 수신 동의
+	MARKETING_CONSENT,  // 마케팅 수신 동의
+	FAQ                 // 이용가이드 / FAQ
 }
