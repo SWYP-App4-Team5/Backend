facb45a09c25e29b2ba63babdc408cc405fae12c
beetrbgus <devbeetrb0214@gmail.com>
2026-04-04 23:56:39 +0900

    hotfix: 중복 sql 삭제 및 user_device 변경

--- src/main/resources/db/migration/V8_motify_user_device_column.sql
+++ src/main/resources/db/migration/V8_motify_user_device_column.sql
@@ -1,7 +1,3 @@
 -- PK 컬럼명 변경
-ALTER TABLE user_device
-    CHANGE COLUMN user_device_id device_id BIGINT NOT NULL AUTO_INCREMENT;
-
--- device_type 컬럼 추가
-ALTER TABLE user_device
-    ADD COLUMN device_type VARCHAR(20);
+ALTER TABLE `user_device`
+    CHANGE COLUMN `user_device_id` `device_id` BIGINT NOT NULL AUTO_INCREMENT;
