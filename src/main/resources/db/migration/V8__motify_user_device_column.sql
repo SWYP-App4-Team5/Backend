02c9e6b1dc63bc98bede1533a86355350fba531f
beetrbgus <devbeetrb0214@gmail.com>
2026-04-05 01:37:07 +0900

    HOTFIX : 파일 수정

--- src/main/resources/db/migration/V8__motify_user_device_column.sql
+++ src/main/resources/db/migration/V8__motify_user_device_column.sql
@@ -1,3 +1,20 @@
--- PK 컬럼명 변경
+DROP PROCEDURE IF EXISTS rename_column_if_exists;
+
+DELIMITER $$
+CREATE PROCEDURE rename_column_if_exists()
+BEGIN
+    -- 컬럼이 존재하면 변경 실행
+    IF EXISTS (
+        SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_NAME = 'user_device'
+        AND COLUMN_NAME = 'user_device_id'
+        AND TABLE_SCHEMA = DATABASE()
+    ) THEN
 ALTER TABLE `user_device`
     CHANGE COLUMN `user_device_id` `device_id` BIGINT NOT NULL AUTO_INCREMENT;
+END IF;
+END $$
+DELIMITER ;
+
+CALL rename_column_if_exists();
+DROP PROCEDURE rename_column_if_exists;
