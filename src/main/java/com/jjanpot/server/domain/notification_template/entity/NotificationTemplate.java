4ab691717920f251feeb24833ba623982e9e7628
beetrbgus <devbeetrb0214@gmail.com>
2026-04-06 04:51:18 +0900

    [FEATURE] 유니크 설정 추가

--- src/main/java/com/jjanpot/server/domain/notification_template/entity/NotificationTemplate.java
+++ src/main/java/com/jjanpot/server/domain/notification_template/entity/NotificationTemplate.java
@@ -23,7 +23,7 @@ import lombok.NoArgsConstructor;
 	uniqueConstraints = {
 		@UniqueConstraint(
 			name = "UQ_template_type",
-			columnNames = {"type"}
+			columnNames = {"type" , "sub_type"}
 		)
 	}
 )
